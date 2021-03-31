53bc8c096b920317b13eff8254b4537446218f85
Semsveta <sveta@neganov.com>
2021-03-31 18:36:19 -0400

    fixed top screen

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 import TVShowSearch from './components/TVShowSearch';
 
 function App() {
-  return <Router>(
-    <div className="App">
-      <div className="App-container">
-        <TVShowSearch />
+  return (
+    <Router>
+      <div className="App">
+        <div className="App-container">
+          <TVShowSearch />
+        </div>
       </div>
-    </div>
-  );</Router>
+    </Router>
+  );
 }
 
 export default App;
