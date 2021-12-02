c68bc3cd52fe804a99756c3487b7daa939319f6d
Semsveta <sveta@neganov.com>
2021-12-02 12:49:33 -0500

    final version - cleaned up the code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,1 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
 
-// test('search text box is rendered', () => {
-//     render(<App />);
-//     const element = screen.getAllByPlaceholderText(/Search title/i);
-//     expect(element).toBeInTheDocument();
-// });
